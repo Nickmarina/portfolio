4752f8a9363c4dbfdac04f531754303c7db4f19b
Marina Permiakova <63551081+Nickmarina@users.noreply.github.com>
2021-06-14 22:01:56 +0300

    added my photo and styles

--- src/js/aboutMePage.js
+++ src/js/aboutMePage.js
@@ -1,11 +1,16 @@
 import myInfoTemplate from '../templates/aboutMePage.hbs';
 import skills from '../dataBase/skills.json';
 import refs from './refs';
+import image from '../images/IMG_9220.JPG'
 
 const downloadAboutMePage = () => {
   const markup = myInfoTemplate(skills);
   refs.container.innerHTML = '';
   refs.container.insertAdjacentHTML('beforeend', markup);
+
+  const mainInfoContainer = document.querySelector('.mainInformation');
+  mainInfoContainer.insertAdjacentHTML('afterbegin', `<img class="mainInformation_img" src=${image} alt="my photo" width="250">`);
+  
   refs.homeBtn.classList.add('activePage');
   refs.contactsBtn.classList.remove('activePage');
   refs.projectsBtn.classList.remove('activePage');
